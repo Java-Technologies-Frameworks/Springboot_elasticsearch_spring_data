7198b5e8160e1ab1af779fb10b92852a1f511d22
Prasanna Kumar Jena <Prasanna.Jena@gds.ey.com>
2020-03-04 15:45:06 +0530

    message first

---
+++ src/main/java/com/prasanna/elastic/search/api/SpringbootElasticsearchSpringDataApplication.java
@@ -0,0 +1,43 @@
+package com.prasanna.elastic.search.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prasanna.elastic.search.api.model.Customer;
+import com.prasanna.elastic.search.api.repository.CustomerRepository;
+
+@SpringBootApplication
+@RestController
+public class SpringbootElasticsearchSpringDataApplication {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+	@PostMapping("/saveCustomer")
+	public int saveCustomer(@RequestBody List<Customer> customers) {
+		repository.saveAll(customers);
+		return customers.size();	
+	}
+
+	@GetMapping("/findAll")
+	public Iterable<Customer> findAllCustomers() {
+		return repository.findAll();
+	}
+
+	@GetMapping("/findByFName/{firstName}")
+	public List<Customer> findByFirstName(@PathVariable String firstName) {
+		return repository.findByFirstname(firstName);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticsearchSpringDataApplication.class, args);
+	}
+}
